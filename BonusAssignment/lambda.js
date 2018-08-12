7a185263984ee96b87e9c590e9aaaf84b5cbfc53
divyajoshiqa <42325793+divyajoshiqa@users.noreply.github.com>
2018-08-13 02:53:30 +0530

    test

--- BonusAssignment/lambda.js
+++ BonusAssignment/lambda.js
@@ -5,7 +5,14 @@ console.log("This is for the Bonus Assignment", event);
 	let operation = event.Operation;
 	let result = null;
 	
+			switch(operation) {
+		case "Add":
 			result = event.Digit1 + event.Digit2;
+			break;
+		case "Subtract":
+			result = event.Digit1 - event.Digit2;
+			break;
+			}
 			
 		event.Result = result;
 	callback(null,'Successfully executed');
